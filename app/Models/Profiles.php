201d9011a31f8ccdd2cbf1973356bc29796b2eae
kristian <cloma.kristian@gmail.com>
2021-01-05 17:14:25 +0800

    u

--- app/Models/Profiles.php
+++ app/Models/Profiles.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Profiles extends Model
 {
     use HasFactory;
+	use \App\Http\Traits\UsesUuid;
 	
 	protected $table = 'profiles';
 	
